e1c14028f59ce574db1ef7c262c9ea91db226f2e
Jonas Gunneng Heer <jonas.g.heer@gmail.com>
2019-10-31 22:14:36 +0100

    added startupscript

--- text.go
+++ text.go
@@ -10,4 +10,13 @@ Commands:
   download
 
 Run 'mc COMMAND -help' for more information on a command`
+
+	startupScript = `#!/bin/bash
+cd /root
+apt update -y
+apt install openjdk-8-jre-headless -y
+wget https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar -O minecraft.jar
+java -Xms512M -Xmx1024M -jar minecraft.jar nogui
+sed -i -e s/eula=false/eula=true/g eula.txt
+screen -dmS minecraft java -Xmx1024M -Xms512M -jar minecraft.jar nogui`
 )
